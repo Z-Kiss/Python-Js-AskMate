f8d2cb592da307ef07376a3d226e065470a86ef9
ujvariroland <ujvari.roland20@gmail.com>
2022-08-09 09:46:23 +0200

    update table

---
+++ sample_data/update_table.sql
@@ -0,0 +1,22 @@
+create table users_data
+(
+    "id "     serial
+        constraint users_data_pk
+            primary key,
+    user_name varchar not null,
+    email     varchar not null,
+    password  varchar not null,
+    honor     int,
+    role      varchar not null
+);
+
+create unique index users_data_email_uindex
+    on users_data (email);
+
+create unique index users_data_user_name_uindex
+    on users_data (user_name);
+
+
+
+
+)
