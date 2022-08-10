26f829d22a297de7c000ee8a06c247264a3480bc
zoty <zoltankiss9210@gmail.com>
2022-08-10 15:17:20 +0200

    add accept button

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -36,7 +36,8 @@ CREATE TABLE answer (
     vote_number integer,
     question_id integer,
     message text,
-    image text
+    image text,
+    accepted bool default false
 );
 
 DROP TABLE IF EXISTS public.comment;
