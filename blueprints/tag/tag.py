4fb3d34694fe6092cbc240489cdda30d623ea83f
zotya <zoltankiss9210@gmail.com>
2023-07-02 16:11:47 +0200

    refactored tag logic and endpoints

--- blueprints/tag/tag.py
+++ blueprints/tag/tag.py
@@ -2,24 +2,26 @@ from flask import Blueprint, request, render_template, redirect, url_for
 
 import data_manager
 
-tag_blueprint = Blueprint('tag', __name__)
+tag_blueprint = Blueprint('tag', __name__, url_prefix='/tag')
 
 
-@tag_blueprint.route('/add_tag/<question_id>', methods=['GET', 'POST'])
+@tag_blueprint.get('/<question_id>')
+def show_add_tag_form(question_id):
+    return render_template('add_tag.html', question_id=question_id)
+
+
+@tag_blueprint.post('/<question_id>')
 def add_tag(question_id):
-    if request.method == 'GET':
-        return render_template('add_tag.html', question_id=question_id)
-    elif request.method == 'POST':
-        tags = set(request.form.get('tag').split())
-        data_manager.update_tags(tags)
-        data_manager.add_tags(tags, question_id)
-        return redirect(url_for('show_question', question_id=question_id))
+    tags = set(request.form.get('tag').split())
+    data_manager.update_tags(tags)
+    data_manager.add_tags(tags, question_id)
+    return redirect(url_for('question.show_question', question_id=question_id))
 
 
-@tag_blueprint.route('/delete/<question_id>/<tag_id>/delete_tag')
+@tag_blueprint.get('/delete/<question_id>/<tag_id>/')
 def delete_tag(question_id, tag_id):
     data_manager.delete_tag(question_id, tag_id)
-    return redirect(url_for("show_question", question_id=question_id, view="no"))
+    return redirect(url_for("question.show_question", question_id=question_id, view="no"))
 
 
 @tag_blueprint.route('/tags')
