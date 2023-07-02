e3b9bd90fef9ec0d58f596855b9ebabb4b2dc23d
zotya <zoltankiss9210@gmail.com>
2023-07-02 16:05:10 +0200

    refactored comment logic and endpoints

--- blueprints/comment/comment.py
+++ blueprints/comment/comment.py
@@ -4,45 +4,47 @@ from flask import Blueprint, request, redirect, url_for, render_template
 
 import data_manager
 
-comment_blueprint = Blueprint('comment', __name__)
+comment_blueprint = Blueprint('comment', __name__, url_prefix='/comment')
 
 
-@comment_blueprint.route('/question/<question_id>/new-comment', methods=['POST', 'GET'])
-def new_comment(question_id):
-    if request.method == 'POST':
-        message = request.form.get('message')
-        submission_time = datetime.now()
-        edited_count = 0
-        data_manager.add_comment(question_id, message, submission_time, edited_count)
-        return redirect(url_for('show_question', question_id=question_id))
-    return render_template('answer.html', id=question_id)
+@comment_blueprint.get('/<question_id>')
+@comment_blueprint.get('/<question_id>/<answer_id>')
+def show_new_comment_form(question_id, answer_id=None):
+    return render_template('add_comment.html', question_id=question_id, answer_id=answer_id)
 
 
-# comment to answer
-@comment_blueprint.route("/answer/<answer_id>/new-comment/<question_id>", methods=['POST', 'GET'])
-def new_comment_answer(answer_id, question_id):
-    if request.method == 'POST':
-        message = request.form.get('message')
-        submission_time = datetime.now()
-        data_manager.comment_answer(answer_id, message, submission_time)
-        return redirect(url_for('show_question', question_id=question_id))
-    return render_template('add_comment.html', answer_id=answer_id, question_id=question_id)
+@comment_blueprint.post('/question/<question_id>')
+def add_new_comment_to_question(question_id):
+    message = request.form.get('message')
+    submission_time = datetime.now()
+    data_manager.add_comment_to_question(question_id, message, submission_time)
+    return redirect(url_for('question.show_question', question_id=question_id, vote='no'))
 
 
-@comment_blueprint.route("/comment/<comment_id>/edit-comment/<question_id>", methods=['POST', 'GET'])
-def edit_comment(comment_id, question_id):
-    if request.method == 'POST':
-        message = request.form.get('message')
-        time = datetime.now()
-        data_manager.update_comment(comment_id, message, time)
-        return redirect(url_for("show_question", question_id=question_id))
+@comment_blueprint.post("/answer/<answer_id>/<question_id>")
+def add_new_comment_to_answer(answer_id, question_id):
+    message = request.form.get('message')
+    submission_time = datetime.now()
+    data_manager.add_comment_to_answer(answer_id, message, submission_time)
+    return redirect(url_for('question.show_question', question_id=question_id, vote='no'))
+
+
+@comment_blueprint.get("/edit/<comment_id>/<question_id>")
+def show_edit_comment_form(comment_id, question_id):
     comment = data_manager.get_comment(comment_id)
     return render_template('edit_comment.html', comment=comment, question_id=question_id)
 
 
-@comment_blueprint.route('/delete/question/comment/<comment_id>/<question_id>')
+@comment_blueprint.post("/edit/<comment_id>/<question_id>")
+def edit_comment(comment_id, question_id):
+    message = request.form.get('message')
+    time = datetime.now()
+    data_manager.update_comment(comment_id, message, time)
+    return redirect(url_for("question.show_question", question_id=question_id, vote='no'))
+
+
+
+@comment_blueprint.route('/delete/<comment_id>/<question_id>')
 def delete_comment(comment_id, question_id):
     data_manager.delete_comment(comment_id)
-    return redirect(url_for('show_question', question_id=question_id, view='no'))
-
-
+    return redirect(url_for('question.show_question', question_id=question_id, view='no'))
