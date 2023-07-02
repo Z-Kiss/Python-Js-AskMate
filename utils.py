0f7740c81e7add9b2c4682d4c03591f487e84ebf
zotya <zoltankiss9210@gmail.com>
2023-07-02 13:16:02 +0200

    add login_required wrapper

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+from functools import wraps
+
 import bcrypt
+from flask import session, flash, redirect
+
+import user_data_manager
 
 
 def hash_password(plain_text_password):
@@ -10,3 +15,19 @@ def hash_password(plain_text_password):
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+def login_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        user_id = session.get('user_id')
+        if not user_id:
+            flash('Need to be Logged in!')
+            return redirect('/auth/login')
+        else:
+            user = user_data_manager.get_user_by_id(user_id)
+            if not user:
+                flash('Authentication failed')
+                return redirect('/auth/login')
+        return func(*args, **kwargs)
+    return decorated
