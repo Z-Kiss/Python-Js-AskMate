11e16d58d26d6b5fd7897d516095cd0d312805fd
zotya <zoltankiss9210@gmail.com>
2023-07-02 12:39:31 +0200

    add refactored database structure

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -4,94 +4,63 @@
 
 -- Dumped from database version 9.5.6
 -- Dumped by pg_dump version 9.5.6
-
-ALTER TABLE IF EXISTS ONLY public.question DROP CONSTRAINT IF EXISTS pk_question_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.answer DROP CONSTRAINT IF EXISTS pk_answer_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.answer DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS pk_comment_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.comment DROP CONSTRAINT IF EXISTS fk_answer_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS pk_question_tag_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_question_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.tag DROP CONSTRAINT IF EXISTS pk_tag_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_tag_id CASCADE;
-
-DROP TABLE IF EXISTS public.question;
-CREATE TABLE question (
-    user_name TEXT NOT NULL,
-    id serial NOT NULL,
-    submission_time timestamp without time zone,
-    view_number integer,
-    vote_number integer,
-    title text,
-    message text,
-    image text
+DROP TABLE IF EXISTS users_data CASCADE ;
+create table users_data (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR NOT NULL,
+    email     VARCHAR NOT NULL,
+    password  VARCHAR NOT NULL,
+    honor     INT,
+    role      VARCHAR NOT NULL,
+    registration_date TIMESTAMP WITHOUT TIME ZONE
 );
 
-DROP TABLE IF EXISTS public.answer;
+DROP TABLE IF EXISTS question CASCADE ;
+CREATE TABLE question (
+    id SERIAL PRIMARY KEY,
+    user_name TEXT NOT NULL,
+    user_id INT REFERENCES users_data ON DELETE CASCADE ,
+    submission_time TIMESTAMP WITHOUT TIME ZONE,
+    view_number INT,
+    vote_number INT,
+    title TEXT,
+    message TEXT,
+    image TEXT
+);
+
+DROP TABLE IF EXISTS answer CASCADE ;
 CREATE TABLE answer (
     user_name TEXT NOT NULL,
-    id serial NOT NULL,
-    submission_time timestamp without time zone,
-    vote_number integer,
-    question_id integer,
-    message text,
-    image text,
-    accepted bool default false
+    id SERIAL PRIMARY KEY,
+    submission_time TIMESTAMP WITHOUT TIME ZONE,
+    vote_number INT,
+    question_id INT REFERENCES question(id) ON DELETE CASCADE,
+    message TEXT,
+    image TEXT,
+    accepted BOOLEAN DEFAULT FALSE
 );
 
-DROP TABLE IF EXISTS public.comment;
+DROP TABLE IF EXISTS comment CASCADE ;
 CREATE TABLE comment (
     user_name TEXT NOT NULL,
-    id serial NOT NULL,
-    question_id integer,
-    answer_id integer,
-    message text,
-    submission_time timestamp without time zone,
-    edited_count integer
+    id SERIAL PRIMARY KEY,
+    question_id INT REFERENCES question(id) ON DELETE CASCADE ,
+    answer_id INT REFERENCES answer(id) ON DELETE CASCADE ,
+    message TEXT,
+    submission_time TIMESTAMP WITHOUT TIME ZONE,
+    edited_count INT
 );
 
-
-DROP TABLE IF EXISTS public.question_tag;
-CREATE TABLE question_tag (
-    question_id integer NOT NULL,
-    tag_id integer NOT NULL
-);
-
-DROP TABLE IF EXISTS public.tag;
+DROP TABLE IF EXISTS tag CASCADE ;
 CREATE TABLE tag (
-    id serial NOT NULL,
-    name text UNIQUE
+id SERIAL PRIMARY KEY,
+name TEXT UNIQUE
 );
 
 
-ALTER TABLE ONLY answer
-    ADD CONSTRAINT pk_answer_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT pk_comment_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY question
-    ADD CONSTRAINT pk_question_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY question_tag
-    ADD CONSTRAINT pk_question_tag_id PRIMARY KEY (question_id, tag_id);
-
-ALTER TABLE ONLY tag
-    ADD CONSTRAINT pk_tag_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id) ON DELETE CASCADE ;
-
-ALTER TABLE ONLY answer
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY question_tag
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY comment
-    ADD CONSTRAINT fk_question_id FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY question_tag
-    ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE ;
+DROP TABLE IF EXISTS question_tag CASCADE ;
+CREATE TABLE question_tag (
+question_id INT NOT NULL REFERENCES question(id) ON DELETE CASCADE,
+tag_id INT NOT NULL REFERENCES tag(id)
+);
 
